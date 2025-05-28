10c4d05bf4111ac139c60420a93bd5d04a415178
ihtsham akram <ihtsham.mentorsols@gmail.com>
2025-05-28 19:24:30 +0500

    refactor: simplify Home component by removing Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return (
-    <div className="min-h-dvh bg-amber-200">
-      <Button>Click me</Button>
-    </div>
-  )
+  return <div className="min-h-dvh bg-amber-200">Start development</div>
 }
