4e9e288aafe2006a3f2e4c08246077ebead77d2c
ahmadzaib876 <ahmadzaib687@gmail.com>
2025-05-29 12:40:21 +0500

    remove theme panel add color in global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Theme>
           {children}
-          <ThemePanel />
         </Theme>
         <Theme accentColor="brown"></Theme>
       </body>
