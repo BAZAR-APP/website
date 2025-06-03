603841faab49faa9a3d4a82c39f91215517aed43
ihtsham akram <ihtsham.mentorsols@gmail.com>
2025-06-03 13:48:36 +0500

    feat(auth): implement forget password page and layout feat(auth): create login page with form inputs and button feat(auth): add registration page with form and validation feat(auth): implement account verification page with OTP input feat(auth): add success page for account verification refactor(auth): remove old layout and signup components refactor(explore): delete unused chalet details and explore pages refactor(explore): remove booking and payment components refactor(components): enhance button component with new styles refactor(components): update header component for navigation

--- src/app/[lang]/(auth)/login/page.tsx
+++ src/app/[lang]/(auth)/login/page.tsx
@@ -11,13 +11,13 @@ const Login = () => {
   return (
     <div className='flex flex-col items-center justify-center h-[100%]'>
       <div className='max-w-[360px] flex flex-col gap-[24px]'>
-        <Image src={"/images/Logo.svg"} alt='' className='w-[150px] h-[48px]' />
+        <Image src={"/images/Logo.svg"} alt=''  width={150} height={48} />
         <div className='flex flex-col gap-[12px]'>
           <h2 className='!text-primary font-600 text-[39px]'>Sign In</h2>
           <p className='!text-secondary font-600 text-[16px]'>Welcome back! Log in to manage your bookings and profile.</p>
         </div>
         <CommonInput name="fullName" placeholder='Enter your full name' type='text' label='Full Name' className={'!bg-[#F9FAFB] !text-[#484A4C] !rounded-[8px] !border-none !h-[42px]'} onChange={handleChange} />
-        <CommonInput icon={<Image src={"/images/countryFlag.svg"} alt='' className='w-[16px] h-[16px]' />} prefix='+965' name="phone" className={'!bg-[#F9FAFB] !text-[#484A4C] !rounded-[8px] !border-none !h-[42px]'} placeholder='Enter your phone number' type='number' label='Phone' onChange={handleChange} />
+        <CommonInput icon={<Image src={"/images/countryFlag.svg"} alt=''  width={16} height={16}/>} prefix='+965' name="phone" className={'!bg-[#F9FAFB] !text-[#484A4C] !rounded-[8px] !border-none !h-[42px]'} placeholder='Enter your phone number' type='number' label='Phone' onChange={handleChange} />
         <CommonInput name="password" placeholder='Enter your password' type='password' label='Password' className={'!bg-[#F9FAFB] !text-[#484A4C] !rounded-[8px] !border-none !h-[42px]'} onChange={handleChange} />
         <CheckBox label={"Remember Me"} />
         <CommonButton children={"Continue"} className="w-[360px] h-[48px] bg-[#29397E] text-white gap-2 pt-3 pr-5 pb-3 pl-5 rounded-lg" />
